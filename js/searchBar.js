5ccde7c35bb4e6337d46556af7f428ec30143f30
Greg <gregthue@gmail.com>
2021-12-31 11:28:17 +0100

    fonction search bar finie en js basic

--- js/searchBar.js
+++ js/searchBar.js
@@ -3,18 +3,23 @@ let relevantRecipe = [];
 let filteredRecipe ;
 let ingredientContent ;
 let ingredientList = [] ;
+let relevantIngredient = [] ;
 
 searchBar.addEventListener("keyup", () => {
 
     const recipeContainer = document.querySelector(".recipeList");
     searchBarValue = searchBar.value.toLowerCase() ;
-    
+
+    if (searchBarValue.length == 0) {
+        recipeContainer.innerHTML = "" ;
+        displayRecipe(recipes)
+    }
 
     if (searchBarValue.length < 3) {
         console.log("Veuillez entrer au minimum 3 caractères")
     }
 
-    if (searchBarValue.length >= 3) {
+    if (searchBarValue.length > 2) {
         searchRecipe();
         recipeContainer.innerHTML = "" ;
         displayRecipe(filteredRecipe);
@@ -27,24 +32,30 @@ searchBar.addEventListener("keyup", () => {
 //
 function searchRecipe() {
 
-
     for (let i = 0; i < recipes.length; i++) {
+        for (let u = 0; u < recipes[i].ustensils.length; u++) {
+            for (let j = 0; j < recipes[i].ingredients.length; j++) {
 
-        for (let j = 0; j < recipes[i].ingredients.length; j++) {
+                if (recipes[i].name.toLowerCase().includes(searchBarValue) 
+                || recipes[i].appliance.toLowerCase().includes(searchBarValue) 
+                || recipes[i].ustensils[u].toLowerCase().includes(searchBarValue) 
+                || recipes[i].ingredients[j].ingredient.toLowerCase().includes(searchBarValue)) { //ameliroer le if pour eviter les doublons
+                    relevantRecipe.push(recipes[i])
+                    //console.log(recipes[i])
 
-            if (recipes[i].name.toLowerCase().includes(searchBarValue) || recipes[i].appliance.toLowerCase().includes(searchBarValue) 
-            || ingredientContent.toLowerCase().includes(searchBarValue) || recipes[i].ingredients[j].ingredient.toLowerCase().includes(searchBarValue)) { //ameliroer le if pour eviter les doublons
-                relevantRecipe.push(recipes[i])
-            } 
+                }    
+            }
+            //console.log(recipes[i].ustensils[u])
         }
     }
-    console.log(ingredientList)
 
-    
+
+    // permet de filtrer pour ne pas avoir de doublons
     filteredRecipe = relevantRecipe.filter(function(ele , pos){
         return relevantRecipe.indexOf(ele) == pos;
+        
     }) 
-    
+    console.log(filteredRecipe)
     if (relevantRecipe == undefined){
         console.log("aucune recette trouvée");
     }
