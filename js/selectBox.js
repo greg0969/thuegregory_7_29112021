51c86550b57681ca8e6e82190c14866dc2d9614b
Greg <gregthue@gmail.com>
2022-01-10 17:08:12 +0100

    Correction de l'affichage des elements dans chaque select box

--- js/selectBox.js
+++ js/selectBox.js
@@ -1,90 +1,60 @@
 
-let tempo = null, objTempo, dblClic = false;
 const selectBox = document.querySelectorAll(".selectBox");
 const inputGroup = document.querySelectorAll(".form-select");
-const chevron = document.querySelectorAll(".fa-chevron-down");
+const chevron = document.querySelectorAll(".chevron");
+const selectedOption = document.querySelectorAll(".selectedOption");
+
+
 
 function displaySelectBox() {
+    let isAriaPopUp = "false" ;
     for (let i = 0; i < inputGroup.length; i++) {
         const liste = document.querySelectorAll(".listeItem");
-        const selectedOption = document.querySelectorAll(".selectedOption");
-    
-        chevron[i].addEventListener("click", () => {
+        
+        let placeHolder = inputGroup[i].placeholder;
+        let tempo = null ;
 
-            if (liste[i].style.display == "" || liste[i].style.display == "none") {
-                liste[i].style.display = "flex";
-                selectedOption[i].style.width = "26em";
-                chevron[i].className = "fas fa-chevron-up";
-                inputGroup[i].style.borderColor = "#68d9a4";
-
-            }
-            if (tempo != null) {
-                clearTimeout(tempo);
-
-            }
-    
+        inputGroup[i].addEventListener("click", () => {
+            
             if (inputGroup[i].readOnly == true) {
                 inputGroup[i].readOnly = false;
                 inputGroup[i].focus();
             }
-                
-    
+
+            if (isAriaPopUp == "false") {
+                liste[i].style.display  = "flex" ;
+                selectedOption[i].setAttribute("aria-haspopup","true");                
+                selectedOption[i].style.width = "33.5em";
+                chevron[i].className = "fas fa-chevron-up chevron";
+                inputGroup[i].placeholder = "Rechercher un "+ placeHolder.toLowerCase().replace(/.$/, '');
+                inputGroup[i].style.fontStyle = "italic" ;
+                isAriaPopUp = "true"
+            }
+   
             else {
-                liste[i].style.display = "none";
+                isAriaPopUp = "false"
+                liste[i].style.display  = "none";
                 selectedOption[i].style.width = "unset"
-                chevron[i].className = "fas fa-chevron-down";
+                selectedOption[i].setAttribute("aria-haspopup","false");
+                chevron[i].className = "fas fa-chevron-down chevron";
+                inputGroup[i].placeholder = placeHolder;
+                inputGroup[i].style.fontStyle = "initial" ;
+
                 inputBlur(inputGroup[i]);
                 tempo = null;
+            }          
 
+            function inputBlur() {
+    
+                if (inputGroup[i].readOnly == false) {
+                    inputGroup[i].readOnly == true;
+                }
             }
            
-        })
-    
-        
-        function mouseClicTempo() {
-            document.getElementById("ingredients").value = objTempo.value;
-            document.getElementById("appareils").value = objTempo.value;
-            document.getElementById("ustensils").value = objTempo.value;
-            gestionListe();
-            tempo = null;
-        }
-
-        function mouseClic() {
-
-            objTempo = inputGroup[i];
-
-            if (tempo == null && inputGroup[i].readOnly == true) {
-                tempo = setTimeout("mouseClicTempo()", "300");
-            }
-
-        }
-
-
-
-        function inputBlur() {
-
-            if (inputGroup[i].readOnly == false) {
-                inputGroup[i].readOnly = true;
-            }
-
-        }
-
-        function mouseOver() {
-            inputGroup[i].className = "item itemOver";
-
-
-        }
-
-        function mouseOut() {
-            inputGroup[i].className = "item itemOut";
-
-
-        }
+        });      
      
-    }
-    
+    }  
 }
-
 
 function displayIngredient() {
     const ingredientDropdown = document.querySelector(".listeItemIngredient")
@@ -174,13 +144,15 @@ let applianceFilter ;
 function addTag(currentTag) {
     const recipeContainer = document.querySelector(".recipeList");
     const tagListContainer = document.querySelector(".currentTagList");
-    const tagContainer = document.createElement("p");
-    tagContainer.classList.add("filter-style");
-    tagListContainer.appendChild(tagContainer);
+    const tagContainer = document.createElement("div");
+
+    tagContainer.classList.add("filter-style",);
+    tagListContainer.appendChild(tagContainer );
     tagContainer.style.backgroundColor = "#3282f7";
     tagContainer.innerHTML = currentTag ;
+
     tagContainer.addEventListener("click", () => {
-        tagContainer.style.display = "none";
+        tagListContainer.removeChild(tagContainer)
         recipeContainer.innerHTML = "" ;
         displayRecipe(recipes);
     });
